3d2bd1568a5e2fb4cf93f5e7f8228ca4a09a84bc
erg55 <erg55@cornell.edu>
2022-04-08 14:28:00 -0400

    Update final_alibaseq_and_align.sh

--- final_alibaseq_and_align.sh
+++ final_alibaseq_and_align.sh
@@ -12,8 +12,15 @@ f='300'
 # set thread number
 t='32'
 
+
 #alibaseq path
-ali_path='/home/FCAM/egordon/alibaseq/alibaseq.py'
+#Check if set in wrapper script and if so do not override
+if [ -z ${ali_path+x} ];
+ then echo "var was unset"
+ ali_path='/home/FCAM/egordon/alibaseq/alibaseq.py'; 
+ else echo "alibaseq path is set to '$ali_path'"; 
+fi
+
 
 
 #Usage message
